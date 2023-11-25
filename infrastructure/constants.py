6c22ac320f499158af1084ea2191c8adca6b5c63
Curtis Robords <curtis.robords@gmail.com>
2023-11-25 17:32:00 -0500

    adding constants

--- infrastructure/constants.py
+++ infrastructure/constants.py
@@ -1,6 +1,5 @@
-CLOUDFLARE_ACCOUNT_ID="5eb48ee94d0e68303e7e386653a1f6cf"
 BUILD_CONFIG = {
-    "BUILD_CMD":"npx @cloudflare/next-on-pages@1",
-    "DESTINATION_DIR":"/.vercel/output/static",
-    "ROOT_DIR":"/frontend"
+    "BUILD_CMD": "npx @cloudflare/next-on-pages@1",
+    "DESTINATION_DIR": "/.vercel/output/static",
+    "ROOT_DIR": "/frontend"
 }
