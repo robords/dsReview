d5a9f1a3a38f3c99779c205ad5f8b69595edc2e9
Curtis Robords <curtis.robords@gmail.com>
2023-11-25 14:57:23 -0500

    adding pulumi for pages

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -3,4 +3,33 @@
 import pulumi
 import pulumi_cloudflare as cloudflare
 
+import constants
 
+# Pages project managing all configs
+deployment_configs = cloudflare.PagesProject("deploymentConfigs",
+                                             account_id=constants.CLOUDFLARE_ACCOUNT_ID,
+                                             build_config=cloudflare.PagesProjectBuildConfigArgs(
+                                                 build_command=constants.BUILD_CONFIG["BUILD_CMD"],
+                                                 destination_dir=constants.BUILD_CONFIG["DESTINATION_DIR"],
+                                                 root_dir=constants.BUILD_CONFIG["ROOT_DIR"],
+                                             ),
+                                             deployment_configs=cloudflare.PagesProjectDeploymentConfigsArgs(
+                                                 preview=cloudflare.PagesProjectDeploymentConfigsPreviewArgs(
+                                                     compatibility_flags=["nodejs_compat"],
+                                                 ),
+                                                 production=cloudflare.PagesProjectDeploymentConfigsProductionArgs(
+                                                     compatibility_flags=["nodejs_compat"],
+                                                 ),
+                                             ),
+                                             name="dsREview",
+                                             production_branch="main",
+                                             source=cloudflare.PagesProjectSourceArgs(
+                                                 config=cloudflare.PagesProjectSourceConfigArgs(
+                                                     deployments_enabled=True,
+                                                     owner="curtis.robords@gmail.com",
+                                                     production_branch="main",
+                                                     production_deployment_enabled=True,
+                                                     repo_name="dsReview",
+                                                 ),
+                                                 type="github",
+                                             ))
