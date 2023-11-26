088950c56c9d601bc017ddf0b8b950e4145568dc
Curtis Robords <curtis.robords@gmail.com>
2023-11-26 15:04:37 -0500

    fixing background image for mobile view

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main>
+        <div className={styles.background_image_style}>
           <img src="/Homepage.png" alt="Home" />
+        </div>
       </main>
     </div>
   );
