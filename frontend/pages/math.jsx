96bf353b5ee8ceb3f9ff9edfc16f2f2500046edc
Curtis Robords <curtis.robords@gmail.com>
2023-12-02 17:51:12 -0500

    fixing svg naming

--- frontend/pages/math.jsx
+++ frontend/pages/math.jsx
@@ -1,17 +1,15 @@
 import styles from '../styles/Home.module.css';
 import SVGImage from '../components/SVGImage';
 import Footer from '../components/Footer';
-import mathsvg from '../public/Math and Statistics.svg';
 
 
 export default function Math() {
-    //style={{ backgroundImage: `url(${mathsvg})` }}
     return (
         <div className={styles.container}>
             <main>
                 <div className={styles.math_background_image_style}
                      align="center" >
-                    <SVGImage src="/Math and Statistics.svg" />
+                    <SVGImage src="/MathStatistics.svg" />
                     <Footer/>
                 </div>
             </main>
