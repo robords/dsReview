c56c9abbbc0458e8302b1409412548b12d6b6fa5
Curtis Robords <curtis.robords@gmail.com>
2023-12-02 19:43:26 -0500

    updating homepage svg

--- frontend/pages/math.jsx
+++ frontend/pages/math.jsx
@@ -2,15 +2,16 @@ import styles from '../styles/Home.module.css';
 import SVGImage from '../components/SVGImage';
 import Footer from '../components/Footer';
 
-
 export default function Math() {
+    //https://dsreview.pages.dev"
+    //[href='https://dsreview.pages.dev/math']
 
     return (
         <div className={styles.container}>
             <main>
                 <div className={styles.math_background_image_style}
                      align="center" >
-                    <SVGImage src="/MathStatistics.svg" />
+                    <SVGImage src="/MathStatistics.svg"/>
                     <Footer/>
                 </div>
             </main>
