5b7536cec91e5510532de62cb36e2426d23c1899
Curtis Robords <curtis.robords@gmail.com>
2023-12-02 20:20:34 -0500

    updating links based on env

--- frontend/components/SVGImage.js
+++ frontend/components/SVGImage.js
@@ -6,8 +6,20 @@ export default function SVGImage({ src }) {
     useEffect(() => {
         // Dynamically import SVG as URL
         const importSVG = async () => {
-            const svgData = await fetch(src).then(data => data.text());
-            document.getElementById('svg').innerHTML = svgData;
+            const env = process.env.NODE_ENV;
+            const svgData = await fetch(src)
+                .then(data => data.text());
+            if (env === "development") {
+                const updatedUrl = "http://localhost:3000"
+                let updatedSvgData;
+                updatedSvgData = svgData.replaceAll("https://dsreview.pages.dev", updatedUrl);
+                document.getElementById('svg').innerHTML = updatedSvgData;
+            } else {
+                const updatedUrl = "https://dsreview.pages.dev"
+                let updatedSvgData;
+                updatedSvgData = svgData.replaceAll("https://dsreview.pages.dev", updatedUrl);
+                document.getElementById('svg').innerHTML = updatedSvgData;
+            }
         }
         importSVG();
     }, [src])
