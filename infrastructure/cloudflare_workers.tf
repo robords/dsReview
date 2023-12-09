bcd03dd549b50c90437687731c8669196474f351
Curtis Robords <curtis.robords@gmail.com>
2023-12-08 19:41:50 -0500

    working on adding a worker route

--- infrastructure/cloudflare_workers.tf
+++ infrastructure/cloudflare_workers.tf
@@ -7,6 +7,14 @@ resource "cloudflare_worker_script" "dsreview_api" {
 
 resource "cloudflare_worker_route" "catch_all_route" {
   zone_id = var.zone_id
-  pattern = "${var.zone}/api/*"
+  pattern = "*${var.zone}/api/*"
   depends_on = [cloudflare_worker_script.dsreview_api]
 }
+
+/* Next:
+Configure Cloudflare Page Rules:
+1. Go to the Cloudflare dashboard and navigate to the "Page Rules" section.
+2. Add a new page rule for your domain.
+3. Set the pattern to match the specific route (e.g., example.com/specific-route/*).
+4. Add the "Forwarding URL" setting and enter the URL of your Cloudflare Pages site.
+*/
--- infrastructure/dsreview_api.js
+++ infrastructure/dsreview_api.js
@@ -1,5 +1,13 @@
 addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest());
+    const url = new URL(event.request.url);
+
+    // Check if the request is for a specific route
+    if (url.pathname.startsWith('/api')) {
+        event.respondWith(handleRequest());
+    } else {
+        // Pass through to Cloudflare Pages for other routes
+        event.respondWith(fetch(event.request));
+    }
 });
 
 async function handleRequest() {
@@ -10,6 +18,7 @@ async function handleRequest() {
     return new Response(JSON.stringify(response), {
         headers: {
             "content-type": "application/json;charset=UTF-8",
+            status: 200
         },
     });
 }
