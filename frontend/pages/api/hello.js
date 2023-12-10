1d670a5c00a9f506225eebe08992738db9a60be0
Curtis Robords <curtis.robords@gmail.com>
2023-12-10 17:11:50 -0500

    edge runtime

--- frontend/pages/api/hello.js
+++ frontend/pages/api/hello.js
@@ -1,4 +1,18 @@
-export const runtime = 'edge';
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Next.js!' })
+import { NextRequest } from 'next/server';
+export const config = {
+    runtime: 'edge',
+};
+
+export default async function handler(req) {
+    return new Response(
+        JSON.stringify({
+            message: 'Hello from Next.js!',
+        }),
+        {
+            status: 200,
+            headers: {
+                'content-type': 'application/json',
+            },
+        },
+    );
 }
